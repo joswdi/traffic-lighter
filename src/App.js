bbf8146e5724085b0e9b9c440dc5c4b8ea299cb7
joswdi <kovalchuksasha2207@gmail.com>
2024-11-14 15:00:14 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [color, setColor] = useState('red')
   return (
     <div className="App">
+      <div className='redColor' style={{ opacity: color === 'red' ? 1 : 0.3}}></div>
+      <div className='yellowColor' style={{ opacity: color === 'yellow' ? 1 : 0.3}}></div>
+      <div className='greenColor' style={{ opacity: color === 'green' ? 1 : 0.3}}></div>
+      <button onClick={() => {
+        if (color === 'red') {
+          setColor('yellow');
+        } else if (color === 'yellow') {
+          setColor('green');
+        } else setColor('red');
+      }}>Change color</button>
     </div>
   );
 }
